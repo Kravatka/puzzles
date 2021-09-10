5415e9439629b70b8876fb8d8621ebd3dacd7205
Kravatka <yegorkrava@gmail.com>
2021-09-10 22:26:08 +0300

    Update createPhoneNumber(6).php

--- createPhoneNumber(6).php
+++ createPhoneNumber(6).php
@@ -1,17 +1,5 @@
 <?php
 
-function createPhoneNumber($numbersArray) {
-  $string = '(';
-  
-  for($i = 0; $i < count($numbersArray); $i++) {
-    if ($i == 2) {
-      $string .= $numbersArray[$i] . ') ';
-    } elseif ($i == 5) {
-      $string .= $numbersArray[$i] . '-';
-    } else {
-      $string .= $numbersArray[$i];
-    }
-  }
-  
-  return $string;
+function createPhoneNumber(array $digits): string {
+  return sprintf("(%d%d%d) %d%d%d-%d%d%d%d", ...$digits);
 }
