1626ac56432ce49f5c43da379fff8388df5587c2
Kravatka <yegorkrava@gmail.com>
2021-09-10 09:00:20 +0300

    Update likes.php

--- likes.php
+++ likes.php
@@ -1,15 +1,16 @@
 <?php
 
 function likes( $names ) {
-  $countNames = count($names);
-  $sufix = " like this";
-  $multi_sufix = " likes this";
-  
-  switch($countNames){
-    case 0: return 'no one' . $multi_sufix; break;
-    case 1: return $names[0] . $multi_sufix; break;
-    case 2: return $names[0] . ' and ' . $names[1] . $sufix; break;
-    case 3: return $names[0] . ', ' . $names[1] . ' and ' . $names[2] . $sufix; break;
-    default: return $names[0] . ', ' . $names[1] . ' and ' . ($countNames - 2) . ' others' . $sufix;
+  switch (count($names)) {
+    case 0:
+      return 'no one likes this';
+    case 1:
+      return vsprintf('%s likes this', $names);
+    case 2:
+      return vsprintf('%s and %s like this', $names);
+    case 3:
+      return vsprintf('%s, %s and %s like this', $names);
+    default:
+      return sprintf('%s, %s and %d others like this', $names[0], $names[1], count($names) - 2);
   }
 }
